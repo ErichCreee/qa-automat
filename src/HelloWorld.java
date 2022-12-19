4741b89cc76c5547328ee2aa26cdd713ab1f7135
ErichCreee <aptelenmarhash@gmail.com>
2022-12-19 20:07:06 +0200

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println();
         System.out.println("Hello World");
+
     }
 }
