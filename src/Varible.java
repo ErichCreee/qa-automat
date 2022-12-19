b6e79f5ccde96a4ba19aa8d67f935d129d18bccb
ErichCreee <aptelenmarhash@gmail.com>
2022-12-19 21:14:23 +0200

    Create Varible.java

---
+++ src/Varible.java
@@ -0,0 +1,42 @@
+public class Varible {
+    public static void main(String[] args) {
+
+        byte myByte;
+
+        myByte = 6 ;
+
+        byte mybyte = -128;
+
+        int myInt = 123123123;
+
+        System.out.println(myByte);
+
+        short myShort = 2343;
+        System.out.println("myShort " + myShort);
+
+        //myShort.sotv
+        System.out.println("myShort = " + myShort);
+
+        long myLong = 123132311456231286L;
+
+        float myFloat = 4.56f;
+
+        double myDouble = 56.48;
+        //myFloat.sotv and to show all varibles
+        System.out.println("myFloat = " + myFloat);
+
+        boolean isTrue = true;
+        boolean isFalse = false;
+
+        char myChar = '\u1F60';
+        System.out.println("myChar = " + myChar);
+
+        String myName = "Nikita";
+
+        System.out.println("myName = " + myName);
+
+    }
+
+    }
+
+
