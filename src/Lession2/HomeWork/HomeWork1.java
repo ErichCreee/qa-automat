6906c89b1303e0cfbc39f33e3151f6a3be3207a5
ErichCreee <aptelenmarhash@gmail.com>
2022-12-20 20:45:36 +0200

    Create HomeWork1.java

---
+++ src/Lession2/HomeWork/HomeWork1.java
@@ -0,0 +1,42 @@
+package Lession2.HomeWork;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+        //Task 1
+        byte myByteF = 3;
+        byte myByteS = -6;
+        System.out.println("myByteF = " + myByteF);
+        System.out.println("myByteS = " + myByteS);
+        short myShortF = 106;
+        short myShortS = -96;
+        System.out.println("myShortF = " + myShortF);
+        System.out.println("myShortS = " + myShortS);
+        int myIntF = 654741;
+        int myIntS = -963456;
+        System.out.println("myIntF = " + myIntF);
+        System.out.println("myIntS = " + myIntS);
+        long myLongF = 69852168763L;
+        long myLongS = -85265479315L;
+        System.out.println("myLongF = " + myLongF);
+        System.out.println("myLongS = " + myLongS);
+        double myDoubleF = 321852.47826;
+        double myDoubleS = -145.6985;
+        System.out.println("myDoubleF = " + myDoubleF);
+        System.out.println("myDoubleS = " + myDoubleS);
+        boolean isTrue = true;
+        boolean isFalse = false;
+        System.out.println("isTrue = " + isTrue);
+        System.out.println("isFalse = " + isFalse);
+        char charS = '\123';
+        char charA = 'A';
+        char charStrangeRussianLetter = '\u0401';
+        char charTen = 1046;
+        char charSixteen = 0x0950;
+        System.out.println("charS = " + charS);
+        System.out.println("charA = " + charA);
+        System.out.println("charStrangeRussianLetter = " + charStrangeRussianLetter);
+        System.out.println("charTen = " + charTen);
+        System.out.println("charSixteen = " + charSixteen);
+
+    }
+}
