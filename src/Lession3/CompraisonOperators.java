bbc7d9dc600550a05e116826764970dae5273438
ErichCreee <aptelenmarhash@gmail.com>
2022-12-22 21:47:50 +0200

    Create CompraisonOperators.java

---
+++ src/Lession3/CompraisonOperators.java
@@ -0,0 +1,23 @@
+package Lession3;
+
+public class CompraisonOperators {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 6;
+        int c = 6;
+
+        System.out.println("a == b " + (a == b));
+        System.out.println("b == c " + (b == c));
+
+        System.out.println("a <= b " + (a <= b));
+        System.out.println("с <= b " + (c <= b));
+        System.out.println("b <= a " + (b <= a));
+        System.out.println("b != a " + (b != a));
+        System.out.println("b != c " + (b != c));
+
+        System.out.println("!true = " + !true);
+        System.out.println("!false = " + !false);
+
+    }
+}
