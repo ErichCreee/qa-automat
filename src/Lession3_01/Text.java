0a8fa971ce5781e78fc26d064c0679d495b3d71e
ErichCreee <aptelenmarhash@gmail.com>
2023-01-02 18:02:32 +0200

    Create Text.java

---
+++ src/Lession3_01/Text.java
@@ -0,0 +1,15 @@
+package Lession3_01;
+
+public class Text {
+    public static void main(String[] args) {
+    // ничего не успел, пересмотреть видео
+
+        String name = "Nikita";
+        String secondName = "Nikita";
+        String thridName = "NIkiTA";
+//        String new String = "Nikita"; !!!!
+
+        boolean areEqual = secondName.equals(name);
+        System.out.println("areEqual = " + areEqual);
+    }
+}
