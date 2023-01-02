5fc28da01414ee660d62c05ffd130abd19571c6f
ErichCreee <aptelenmarhash@gmail.com>
2023-01-02 20:58:07 +0200

    Create ArrayExemption.java

---
+++ src/lession5/ArrayExemption.java
@@ -0,0 +1,10 @@
+package lession5;
+
+public class ArrayExemption {
+    public static void main(String[] args) {
+        int array[] = {1,2,3};
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
