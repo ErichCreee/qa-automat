8a796944a580ed94b8e8bdf1920f31e25b9de36e
ErichCreee <aptelenmarhash@gmail.com>
2023-01-02 18:10:18 +0200

    Update random.java

--- src/lession5/random.java
+++ src/lession5/random.java
@@ -2,19 +2,25 @@ package lession5;
 
 import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class random {
     public static void main(String[] args) {
 
-
         Random random = new Random();
-        int randomInteger = random.nextInt();
+        ThreadLocalRandom threadLocalRandom = ThreadLocalRandom.current();
+
+       /* int randomInteger = random.nextInt();
         int randomIntegerWithBound = random.nextInt(10);
         double randomDouble = random.nextDouble();
 
         System.out.println("randomInteger = " + randomInteger);
         System.out.println("randomIntegerWithBound = " + randomIntegerWithBound);
-        System.out.println("randomDouble = " + randomDouble);
+        System.out.println("randomDouble = " + randomDouble);*/
+
+        int randomIntegerWithBound = threadLocalRandom.nextInt(10,20);
+        System.out.println("randomIntegerWithBound = " + randomIntegerWithBound);
+        
     }
 
 
